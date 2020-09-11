72da7b246b9e608409600e46ab49feec892535b9
Vinh Mao <vinhxmao87@gmail.com>
2020-09-10 20:24:29 -0700

    Main page code created, formula folders intitated

---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import QuadFormula from "../components/QuadFormula";
+import VolumeFormula from "../components/VolumeFormula";
+
+function Main() {
+    return (
+        <div>
+            <QuadFormula />
+            <VolumeFormula />
+        </div>
+    );
+}
+
+export default Main;
