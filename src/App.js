54e57d77b2414d53c9416d3c43a8bc6ad9a7ee66
Vinh Mao <vinhxmao87@gmail.com>
2020-09-23 15:41:22 -0700

    fixing errors for displaying page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from "./pages/Main";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
---
+++ src/components/QuadFormula/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import formulas from "../../utils/formulas";
+
+function QuadFormula(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default QuadFormula;
