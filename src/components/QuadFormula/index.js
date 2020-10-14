6c167b0466bddb088e8b8651521d3eaeb53ab29c
Vinh Mao <vinhxmao87@gmail.com>
2020-10-13 23:24:08 -0700

    discriminant equation added

--- src/components/QuadFormula/index.js
+++ src/components/QuadFormula/index.js
@@ -7,6 +7,7 @@ function QuadFormula(props) {
             <p>{props.a < 0 ? "-" : null}{props.a !== 1 ? props.a : null}x<sup>2</sup> {!props.b < 0 ? "+" : null} {props.b !== 1 ? props.b : null}x {!props.c < 0 ? "+" : null} {props.c !== 1 ? props.c : null}</p>
             <p>{formulas.quadFormula(props.a, props.b, props.c)[0]}</p>
             <p>{formulas.quadFormula(props.a, props.b, props.c)[1]}</p>
+            <p>{formulas.discriminant(props.a, props.b, props.c)}</p>
         </div>
     );
 }
--- src/utils/otherFormulas.js
+++ src/utils/otherFormulas.js
@@ -6,6 +6,17 @@ export default {
         return [num1, num2];
     },
 
+    discriminant: function (a, b, c) {
+        const disc = Math.pow(b, 2) - (4 * a * c);
+        if (disc > 0) {
+            return "There are 2 real roots.";
+        } else if (disc === 0) {
+            return "There is a repeated root.";
+        } else {
+            return "There are 2 complex roots.";
+        }
+    },
+
     heronFormula: function (a, b, c) {
         const s = (a + b + c) / 2;
         return Math.sqrt(s * (s - a) * (s - b) * (s - c));
